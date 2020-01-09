209447578520a9cfa2340b708fad20fec1490d37
Julien CHEDAL-ANGLAY <jchedal-@e3r6p5.42.fr>
2020-01-09 01:46:09 +0100

    Add bonus1

---
+++ bonus1/source.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+    int n = atoi(argv[1]);
+    char buf[40]; /* equivalent to int buf[10] */
+        
+    if (n <= 9)
+    {
+        memcpy(buf, argv[2], n * 4);
+        if (n == 0x574f4c46)
+        {
+            execl("/bin/sh", "sh", NULL);
+        }
+        return 0;
+    }
+    return 1;
+}
